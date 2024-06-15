91f8214a340e30162e1500c97801b60c72546b29
syahrul anwar <syahrulanwar83@gmail.com>
2024-06-15 11:26:34 +0800

    sqlite

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('blog.home');
 });
+
+
+Route::get('/visi', function () {
+    return view('blog.visi-misi');
+});
