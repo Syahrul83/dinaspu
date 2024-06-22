4e4f6d56b1c0961087e8f162f3da06ba0bfb3f19
syahrul anwar <syahrulanwar83@gmail.com>
2024-06-22 19:44:10 +0800

    kontak

---
+++ app/Filament/Resources/KontakResource.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\KontakResource\Pages;
+use App\Filament\Resources\KontakResource\RelationManagers;
+use App\Models\Kontak;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class KontakResource extends Resource
+{
+    protected static ?string $model = Kontak::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('title')
+                    ->required(),
+                Forms\Components\TextInput::make('alamat')
+                    ->required(),
+                Forms\Components\TextInput::make('email')
+                    ->email()
+                    ->required(),
+                Forms\Components\TextInput::make('no_hp')
+                    ->required(),
+                Forms\Components\TextInput::make('sda_tv')
+                    ->required(),
+                Forms\Components\TextInput::make('twitter')
+                    ->required(),
+                Forms\Components\TextInput::make('youtube')
+                    ->required(),
+                Forms\Components\TextInput::make('instagram')
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('title')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('alamat')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('email')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('no_hp')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('sda_tv')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('twitter')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('youtube')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('instagram')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListKontaks::route('/'),
+            'create' => Pages\CreateKontak::route('/create'),
+            'edit' => Pages\EditKontak::route('/{record}/edit'),
+        ];
+    }
+}
