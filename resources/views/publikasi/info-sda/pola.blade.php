5c1ebc2e87662931022775e120b5bee593d2f888
hardygraph <hardygraph@gmail.com>
2024-07-01 20:18:08 +0800

    pesan commit

--- resources/views/publikasi/info-sda/pola.blade.php
+++ resources/views/publikasi/info-sda/pola.blade.php
@@ -1,6 +1,7 @@
 @extends('layout.app')
 @section('content')
 
-
+<h4 class="text-center mt-2">Dokumen Pola Pengelolaan Sumber Daya Air Wilayah Sungai Mahakam</h4>
+                <embed type="application/pdf" src="./doc/Pola-Mahakam.pdf" width="100%" height="600"></embed>
 
 @endsection
--- resources/views/publikasi/info-sda/rpsda.blade.php
+++ resources/views/publikasi/info-sda/rpsda.blade.php
@@ -1,6 +1,8 @@
 @extends('layout.app')
 @section('content')
 
+<h4 class="text-center mt-2">Dokumen Recana Pengelolaan Sumber Daya Air Wilayah Sungai Mahakam</h4>
+                <embed type="application/pdf" src="./doc/RPSDA-Mahakam.pdf" width="100%" height="600"></embed>
 
 
 @endsection
