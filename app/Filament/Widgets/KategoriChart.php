b1ae3b0c18342a60ff058b9a9b169236b6edde26
syahrul anwar <syahrulanwar83@gmail.com>
2024-07-04 18:34:40 +0800

    chart final

---
+++ app/Filament/Widgets/KategoriChart.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\Post;
+use Filament\Widgets\DoughnutChartWidget;
+
+class KategoriChart extends DoughnutChartWidget
+{
+    protected static ?string $heading = 'Kategori Post';
+    protected static ?string $maxHeight = '260px';
+
+    protected function getData(): array
+    {
+
+        $counts = Post::join('kategoris', 'kategoris.id', '=', 'posts.kategori_id')
+            ->groupBy('posts.kategori_id')
+            ->selectRaw('count(posts.id) as count, kategoris.title as label')
+            ->get();
+
+        // Function to generate a random RGB color
+        function generateRandomColor()
+        {
+            return 'rgb(' . rand(0, 255) . ',' . rand(0, 255) . ',' . rand(0, 255) . ')';
+        }
+
+        $backgroundColor = [];
+
+        // Generate 10 random colors
+        for ($i = 0; $i < 10; $i++) {
+            $backgroundColor[] = generateRandomColor();
+        }
+
+
+        return [
+            'datasets' => [
+                [
+                    'label' => 'Kategori',
+                    'data' => $counts->pluck('count')->toArray(),
+                    'backgroundColor' => $backgroundColor,
+                    'hoverOffset' => 4
+                ],
+            ],
+            'labels' => $counts->pluck('label')->toArray(),
+        ];
+
+    }
+
+    protected function getType(): string
+    {
+        return 'doughnut';
+    }
+}
--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -38,7 +38,7 @@ class AdminPanelProvider extends PanelProvider
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
                 Widgets\AccountWidget::class,
-                Widgets\FilamentInfoWidget::class,
+                // Widgets\FilamentInfoWidget::class,
             ])
             ->middleware([
                 EncryptCookies::class,
@@ -52,8 +52,8 @@ class AdminPanelProvider extends PanelProvider
                 DispatchServingFilamentEvent::class,
             ])
             ->navigationGroups([
-                'Halaman Depan',
                 'Berita & Informasi',
+                'Halaman Depan',
                 'Menu & Halaman',
                 'Publikasi',
                 'Settings',
