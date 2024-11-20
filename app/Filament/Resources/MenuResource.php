32332db08f9d8ed657be81080ba22fdfd25b4f40
syahrul anwar <syahrulanwar83@gmail.com>
2024-11-20 20:28:25 +0800

    main final

--- app/Filament/Resources/MenuResource.php
+++ app/Filament/Resources/MenuResource.php
@@ -3,6 +3,8 @@
 
 namespace App\Filament\Plugins\Resources;
 
+
+use Illuminate\Support\Facades\Auth;
 use Datlechin\FilamentMenuBuilder\Resources\MenuResource as BaseMenuResource;
 
 class MenuResource extends BaseMenuResource
@@ -10,9 +12,35 @@ class MenuResource extends BaseMenuResource
     protected static ?string $navigationGroup = 'Menu & Halaman';
 
 
+    public static function canAccess(): bool
+    {
+        if (
+            Auth::user()->roles->pluck('name')[0] == 'admin' or
+            Auth::user()->roles->pluck('name')[0] == 'super_admin'
+        ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    public static function shouldRegisterNavigation(): bool
+    {
+        if (
+            Auth::user()->roles->pluck('name')[0] == 'admin' or
+            Auth::user()->roles->pluck('name')[0] == 'super_admin'
+        ) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
 
     public static function getNavigationBadge(): ?string
     {
+
         return number_format(static::getModel()::count());
     }
 }
