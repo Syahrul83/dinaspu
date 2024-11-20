bca4e8e2f0eec3a980f5c8483345cd1614bf1124
syahrul anwar <syahrulanwar83@gmail.com>
2024-11-20 21:07:27 +0800

    update baru

--- resources/views/home/publikasi/sda.blade.php
+++ resources/views/home/publikasi/sda.blade.php
@@ -3,13 +3,11 @@
 
 <h3 class="text-center mt-4">{{ $post->title }}</h3>
 <hr>
-@if ($post->file == null or asset('storage/' . $post->file) == null)
+@if (empty($post->file) || !Storage::exists('public/' . $post->file))
     <div class="d-flex justify-content-center align-items-center" style="height: 100vh;">
         <h1>NO DATA PDF</h1>
     </div>
 @else
-    <embed type="application/pdf" src="{{asset('storage/' . $post->file)}}" width="100%" height="600"></embed>
-
+    <embed type="application/pdf" src="{{ asset('storage/' . $post->file) }}" width="100%" height="600"></embed>
 @endif
-
 @endsection
