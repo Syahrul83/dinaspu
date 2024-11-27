7880a3e8737bc55d006dc304c69103572fc5a759
syahrul anwar <syahrulanwar83@gmail.com>
2024-11-27 16:17:35 +0800

    update

--- app/Filament/Resources/KmpsResource.php
+++ app/Filament/Resources/KmpsResource.php
@@ -4,7 +4,7 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\KmpsResource\Pages;
 use App\Filament\Resources\KmpsResource\RelationManagers;
-use App\Models\Kmps;
+use App\Models\kmps;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
